f2403c75e41e031bf2bf6a7d7d605d304f5a73aa
wp-kurila <alendar94@mail.ru>
2019-06-14 00:21:20 +0300

    lesson-3

---
+++ my-app/src/components/post-list/post-list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './post-list.css';
+
+import PostListItem from '../post-list-item';
+
+const PostList = () => {
+    return (
+        <ul className="app-list list-group"> 
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+        </ul>
+    )
+}
+
+export default PostList;
