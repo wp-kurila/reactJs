4a579a64ced9c162f6adffc56bb5eb67d7e4390f
wp-kurila <alendar94@mail.ru>
2019-06-15 21:33:49 +0300

    lesson 4

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -9,6 +9,14 @@ import PostAddForm from '../post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 1},
+        {label: 'That is so good', important: false, id: 2},
+        {label: 'I need a break..', important: false, id: 3},
+        5
+    ];
+
     return (
         <div className="app">
             <AppHeader />
@@ -16,7 +24,7 @@ const App = () => {
                 <SearchPanel />
                 <PostStatusFilter />
             </div>
-            <PostList />
+            <PostList posts={data} />
             <PostAddForm />
         </div>
     )
--- my-app/src/components/post-list/post-list.js
+++ my-app/src/components/post-list/post-list.js
@@ -4,12 +4,23 @@ import './post-list.css';
 
 import PostListItem from '../post-list-item';
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+    const elements = posts.map( (item) => {
+        
+        if (typeof item == "object") {
+            const {id, ...itemProps} = item
+            return (
+                <li key={id} className="list-group-item">
+                    <PostListItem {...itemProps}/>
+                </li>
+            )   
+        }        
+    });
+
     return (
         <ul className="app-list list-group"> 
-            <PostListItem />
-            <PostListItem />
-            <PostListItem />
+            {elements}
         </ul>
     )
 }
